24bf1896ffc0cb77a005368642ae6d2fa6033397
Алексей Кузнецов <mirakuru@webtoucher.ru>
2013-08-10 11:38:55 +0700

    Хелпер кодирования/декодирования IP-адреса вынес в отдельный класс

---
+++ classes/ip-coder.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Класс для работы с IP-адресом.
+ * Можно использовать для хранения IP-адресов в базе в виде числа,
+ * что ускорит поиск и сократит расходуемое пространство.
+ */
+class IpCoder {
+
+	/**
+	 * Максимальное значение числа, в которое можно конвертировать IP-адрес
+	 */
+	const IP_LONG_MAX = 4294967295;
+
+
+	/**
+	 * Конвертирует IP-адрес в число
+	 *
+	 * @param string $ip IP-адрес
+	 * @return integer|float число
+	 * @throws Exception выкидывает, если строка не соответствует формату 0.0.0.0
+	 */
+	public static function encode($ip) {
+		$ipArr = explode('.', $ip);
+		if (count($ipArr) != 4) {
+			throw new Exception("Неверный IP '$ip'.");
+		}
+		return $ipArr[0] * 0x1000000 + $ipArr[1] * 0x10000 + $ipArr[2] * 0x100 + $ipArr[3];
+	}
+
+	/**
+	 * Конвертирует число в IP-адрес
+	 *
+	 * @param integer|float $long число в диапазоне 0...4294967295
+	 * @return string IP-адрес
+	 * @throws Exception выкидывает, если число не входит в указанный диапазон
+	 */
+	public static function decode($long) {
+		if ($long < 0 || $long > self::IP_LONG_MAX) {
+			throw new Exception("Значение '$long' нельзя преобразовать в верный IP.");
+		}
+		$ipArr = [
+			0b11111111 & $long >> 24,
+			0b11111111 & $long >> 16,
+			0b11111111 & $long >> 8,
+			0b11111111 & $long,
+		];
+		return implode('.', $ipArr);
+	}
+}
--- examples/classes/ip-coder.php
+++ examples/classes/ip-coder.php
@@ -1,15 +1,16 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
-require __DIR__ . '/../../helpers/ip.php';
+require __DIR__ . '/../../classes/ip-coder.php';
+
 
 const SOME_IP = '255.123.2.0';
 
 echo '<pre>';
 
-$ipEncoded = ipToLong(SOME_IP);
+$ipEncoded = IpCoder::encode(SOME_IP);
 echo '<b>КОДИРОВАНИЕ IP</b>' . PHP_EOL . PHP_EOL;
 echo SOME_IP . ' -> ' . $ipEncoded . PHP_EOL . PHP_EOL;
 echo '<b>ДЕКОДИРОВАНИЕ IP</b>' . PHP_EOL . PHP_EOL;
-echo $ipEncoded . ' -> ' . longToIp($ipEncoded);
+echo $ipEncoded . ' -> ' . IpCoder::decode($ipEncoded);
 
 echo '</pre>';
